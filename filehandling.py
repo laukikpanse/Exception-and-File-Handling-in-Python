bf9e3d7d24141a47a09ff0c17f83208c0f478e57
bob <bob@elonmusk.com>
2018-06-12 15:57:00 -0400

    second Commit

--- filehandling.py
+++ filehandling.py
@@ -5,7 +5,10 @@ file = open("demo.txt", 'w')
 
 file.write("Hello Sir how are you..!!")
 
+#now to add new lines we cannot use 'w' instead use append mode 'a'
+file = open("demo.txt", 'a')
 
+file.write("\nThis is the second line")
 
 #to access that file we will use a fileholder
 file = open("demo.txt", 'r') #specify the filename and r denotes readfile
@@ -15,3 +18,5 @@ content = file.read()
 #print the contents onto the console
 print(content)
 
+#always remember to close the file
+file.close()
